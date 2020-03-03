0ddf5458efa1942beafa409189f98da704f5820e
Max Pinheiro <maxpinheiro181@gmail.com>
2020-03-03 09:08:37 -0500

    setGOE method, finshed buttons

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-
-};
+var currentElement = "";
+var currentGOE = 0;
+var totalScore = 0;
 
 function log(message) {
     console.log(message);
@@ -8,10 +8,23 @@ function log(message) {
 
 // called when a navigation tab is clicked: hides all tabs, displays the tab with the id tabName
 function setPage(tabName) {
-    var i;
-    var x = document.getElementsByClassName("tab-content");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    // only swap pages if currently not selecting anything
+    if (this.currentElement == "") {
+        var i;
+        var x = document.getElementsByClassName("tab-content");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(tabName).style.display = "block";
     }
-    document.getElementById(tabName).style.display = "block";
+}
+
+// called when GOE button is clicked: sets the current GOE
+function setGOE(goe) {
+    this.currentGOE = goe;
+    document.getElementById("selected-goe").innerText = "";
+    if (goe > 0) {
+        document.getElementById("selected-goe").innerText = "+ ";
+    }
+    document.getElementById("selected-goe").innerText += this.currentGOE;
 }
